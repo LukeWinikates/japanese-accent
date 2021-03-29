7eb1fe02989d3881c7e5d2cd7562eeaa6909665a
Luke Winikates <luke.winikates@gmail.com>
2021-03-29 07:08:47 -0700

    working 'dictaphone' interaction

--- web/src/Recorder.tsx
+++ web/src/Recorder.tsx
@@ -1,39 +1,66 @@
 import React, {useRef, useState} from 'react';
 
 import {useReactMediaRecorder} from "react-media-recorder";
+import {Button} from "@material-ui/core";
+import {Pagination} from '@material-ui/lab';
+import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
+
+declare type AudioRecording = {
+  blob: Blob,
+  blobUrl: string,
+  timestamp: Date,
+}
 
 export const Recorder = () => {
-  const [showMedia, setShowMedia] = useState(false);
+  const [recordings, setRecordings] = useState<AudioRecording[]>([]);
+  const [page, setPage] = useState(recordings.length > 0 ? recordings.length : null);
+
   const audioRef = useRef<HTMLAudioElement>(null);
+  const handlePageChange = (event: any, page: number) => {
+    audioRef.current?.pause();
+    setPage(page);
+  };
 
   const {
     status,
     startRecording,
     stopRecording,
-    mediaBlobUrl,
   } = useReactMediaRecorder({
     audio: true,
-    onStop: () => {
-      console.log("onstop");
+    onStop: (blobUrl: string, blob: Blob) => {
+      setRecordings([...recordings, {blobUrl, blob, timestamp: new Date()}]);
       audioRef.current?.play();
     }
   });
 
-
   const toggle = () => {
     if (status === 'recording') {
       stopRecording();
-      setShowMedia(true);
+    } else {
+      audioRef.current?.pause();
+      startRecording();
     }
-    startRecording();
   };
 
+  const effectivePage = page ? page : recordings.length;
+  const currentRecording = recordings[effectivePage - 1];
+  const hasRecordings = recordings.length > 0;
 
   return (
     <div>
-      <div>{status}</div>
-      <button onClick={toggle}>{status !== 'recording' ? "start recording" : "stop recording"}</button>
-      {showMedia ? <audio ref={audioRef} src={mediaBlobUrl || ""} controls={true}/> : <span>no media yet</span>}
+      <Button onClick={toggle} color="default" variant="outlined" startIcon={<RadioButtonCheckedIcon/>}>
+        {status !== 'recording' ? "Record" : "Stop"}
+      </Button>
+      {hasRecordings ? <>
+        <div>
+          <audio ref={audioRef} src={currentRecording.blobUrl || ""} controls={true}/>
+          <div>
+            {currentRecording.timestamp.toLocaleString()}
+          </div>
+        </div>
+        <Pagination count={recordings.length} page={page || recordings.length} siblingCount={2} size="small"
+                    onChange={handlePageChange}/>
+      </> : <></>}
     </div>
   );
 };
