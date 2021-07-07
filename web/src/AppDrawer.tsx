0b1ded023c629d5a5f7b8982217f2a4ebc20fd35
Luke Winikates <luke.winikates@gmail.com>
2021-07-07 13:02:56 -0700

    remove junk categories from drawer

--- web/src/AppDrawer.tsx
+++ web/src/AppDrawer.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {Category} from "./api";
 import {makeStyles} from '@material-ui/core/styles';
 import {Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
-import InboxIcon from '@material-ui/icons/Inbox';
-import MailIcon from '@material-ui/icons/Mail';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import NoteIcon from '@material-ui/icons/Note';
@@ -59,15 +57,6 @@ export function AppDrawer({categories, open, handleClose, theme}: AppDrawerProps
           {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
         </IconButton>
       </div>
-      <Divider/>
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
-            <ListItemText primary={text}/>
-          </ListItem>
-        ))}
-      </List>
       <Divider/>
       <List>
         {categories.map((category, index) => (
