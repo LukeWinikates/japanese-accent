83dfa107329bc872a0a6e200ca1a04141dbe7708
Luke Winikates <luke.winikates@gmail.com>
2021-09-09 10:20:00 -0700

    cleanup

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -26,7 +26,6 @@ func Configure(engine *gin.Engine, wordsFilePath, mediaDirPath string, db gorm.D
 
 	media := engine.Group("/media")
 	{
-		//media.GET("/audio/:id/segments", MakeAudioSegmentsGET(mediaDirPath, db))
 		media.POST("/audio/:audioId/segments/:id", MakeAudioSegmentsPOST(db))
 		media.POST("/audio/:audioId/segments/", MakeAudioSegmentsCREATE(db))
 		media.DELETE("/audio/:audioId/segments/:id", MakeAudioSegmentsDELETE(db))
