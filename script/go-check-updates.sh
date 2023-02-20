4cb533ebc5e73187937678eb146c3169a83036c8
Luke Winikates <luke.winikates@gmail.com>
2023-02-20 14:32:14 -0800

    add x/net to eagerly updated go deps

--- script/go-check-updates.sh
+++ script/go-check-updates.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-go get gorm.io/gorm@latest
-go get gorm.io/driver/sqlite@latest
-go get github.com/prometheus/client_golang@latest
 go get github.com/gin-gonic/gin@latest
-go get github.com/u2takey/ffmpeg-go@latest
+go get github.com/prometheus/client_golang@latest
 go get github.com/stretchr/testify@latest
+go get github.com/u2takey/ffmpeg-go@latest
+go get golang.org/x/net@latest
+go get gorm.io/driver/sqlite@latest
+go get gorm.io/gorm@latest
 go mod tidy
 
 git diff --exit-code go.mod go.sum
