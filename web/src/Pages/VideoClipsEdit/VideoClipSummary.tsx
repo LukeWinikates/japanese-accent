62bea2b3ab1c14b75568453b0538e9869c814f46
Luke Winikates <luke.winikates@gmail.com>
2023-02-26 15:00:16 -0800

    Extract VideoClipSummary component

---
+++ web/src/Pages/VideoClipsEdit/VideoClipSummary.tsx
@@ -0,0 +1,80 @@
+import {Box, Grid, LinearProgress, Stack, Typography} from "@mui/material";
+import {msToHumanReadable} from "../../App/time";
+import {MiniWaveform} from "./MiniWaveform";
+import React from "react";
+import {Video, VideoAdvice, Waveform} from "../../App/api";
+
+declare type Props = {
+  video: Video,
+  waveform: Waveform,
+  advice: VideoAdvice
+}
+
+export const VideoClipSummary = ({video, advice, waveform}: Props) => {
+  const draftsDuration = video.segments.map(seg => seg.endMS - seg.startMS).reduce((len, memo) => memo + len, 0);
+  const totalMS = advice.suggestedSegments[advice.suggestedSegments.length - 1].endMS;
+
+  return (
+    <Grid container>
+      <Grid xs={4} item container>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={2}
+        >
+          <Typography>{video.segments.length}</Typography>
+          <Typography>Clips Created</Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={2}
+        >
+          <Typography>{advice.suggestedSegments.length}</Typography>
+          <Typography>Suggested Clips Available to Review</Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={2}
+        >
+          <Typography>{msToHumanReadable(draftsDuration)}</Typography>
+          <Typography>Total Clip Duration</Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={2}
+        >
+          <Typography>{msToHumanReadable(totalMS)}</Typography>
+          <Typography>Source Video Length</Typography>
+        </Stack>
+
+        <Stack>
+          <Box sx={{display: 'flex', alignItems: 'center'}}>
+            <Box sx={{width: '100%', mr: 1}}>
+              <LinearProgress variant="determinate" value={draftsDuration / totalMS}/>
+            </Box>
+            <Box sx={{minWidth: 35}}>
+              <Typography variant="body2" color="text.secondary">{`${Math.round(
+                draftsDuration / totalMS
+              )}%`}</Typography>
+            </Box>
+            <Box>
+              Video
+            </Box>
+          </Box>
+        </Stack>
+
+      </Grid>
+      <Grid item container xs={8}>
+        <MiniWaveform samples={waveform.samples} sampleRate={waveform.sampleRate}
+                      playerPositionMS={0} initWidth={740}/>
+      </Grid>
+    </Grid>
+  );
+}
--- web/src/Pages/VideoClipsEdit/YouTubeVideoEditor.tsx
+++ web/src/Pages/VideoClipsEdit/YouTubeVideoEditor.tsx
@@ -7,22 +7,18 @@ import {
   Card,
   CardContent,
   Container,
-  Grid,
-  LinearProgress,
-  Stack,
   Typography
 } from "@mui/material";
 import {useServerInteractionHistory} from "../../Layout/useServerInteractionHistory";
 import {Timeline} from "./Timeline";
 import {Loadable} from "../../App/loadable";
 import {videoAdviceGET, waveformGET} from "../../App/ApiRoutes";
-import {msToHumanReadable} from "../../App/time";
-import {MiniWaveform} from "./MiniWaveform";
 import {Link} from "react-router-dom";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import LaunchIcon from "@mui/icons-material/Launch";
 import Mic from "@mui/icons-material/Mic";
 import ListenIcon from '@mui/icons-material/Hearing';
+import {VideoClipSummary} from "./VideoClipSummary";
 
 export const YouTubeVideoEditor = ({video, onVideoChange}: { video: Video, onVideoChange: (v: Video) => void }) => {
   const {logError} = useServerInteractionHistory();
@@ -30,7 +26,6 @@ export const YouTubeVideoEditor = ({video, onVideoChange}: { video: Video, onVid
   const [samplesData, setSamplesData] = useState<Loadable<ApiWaveform>>("loading");
 
   useEffect(() => {
-    // Promise.all()
     videoAdviceGET(video.videoId)
       .then(r => setAdvice({data: r.data})).catch(logError);
 
@@ -44,9 +39,6 @@ export const YouTubeVideoEditor = ({video, onVideoChange}: { video: Video, onVid
   if (advice === "loading" || samplesData === "loading") {
     return (<>loading...</>);
   }
-
-  const draftsDuration = video.segments.map(seg => seg.endMS - seg.startMS).reduce((len, memo) => memo + len, 0);
-  const totalMS = advice.data.suggestedSegments[advice.data.suggestedSegments.length - 1].endMS;
 
   let muteSuggestion = (segmentToMute: SuggestedSegment) => {
     let newSuggestions = [...advice.data.suggestedSegments];
@@ -94,67 +86,12 @@ export const YouTubeVideoEditor = ({video, onVideoChange}: { video: Video, onVid
                 what you hear, and compare your speaking to the native recording.
               </Typography>
 
-              <Grid container>
-                <Grid xs={4} item container>
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    alignItems="stretch"
-                    spacing={2}
-                  >
-                    <Typography>{video.segments.length}</Typography>
-                    <Typography>Clips Created</Typography>
-                  </Stack>
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    alignItems="stretch"
-                    spacing={2}
-                  >
-                    <Typography>{advice.data.suggestedSegments.length}</Typography>
-                    <Typography>Suggested Clips Available to Review</Typography>
-                  </Stack>
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    alignItems="stretch"
-                    spacing={2}
-                  >
-                    <Typography>{msToHumanReadable(draftsDuration)}</Typography>
-                    <Typography>Total Clip Duration</Typography>
-                  </Stack>
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    alignItems="stretch"
-                    spacing={2}
-                  >
-                    <Typography>{msToHumanReadable(totalMS)}</Typography>
-                    <Typography>Source Video Length</Typography>
-                  </Stack>
+              <VideoClipSummary
+                video={video}
+                advice={advice.data}
+                waveform={samplesData.data}
+              />
 
-                  <Stack>
-                    <Box sx={{display: 'flex', alignItems: 'center'}}>
-                      <Box sx={{width: '100%', mr: 1}}>
-                        <LinearProgress variant="determinate" value={draftsDuration / totalMS}/>
-                      </Box>
-                      <Box sx={{minWidth: 35}}>
-                        <Typography variant="body2" color="text.secondary">{`${Math.round(
-                          draftsDuration / totalMS
-                        )}%`}</Typography>
-                      </Box>
-                      <Box>
-                        Video
-                      </Box>
-                    </Box>
-                  </Stack>
-
-                </Grid>
-                <Grid item container xs={8}>
-                  <MiniWaveform samples={samplesData.data.samples} sampleRate={samplesData.data.sampleRate}
-                                playerPositionMS={0} initWidth={740}/>
-                </Grid>
-              </Grid>
             </CardContent>
           </Card>
         </Box>
