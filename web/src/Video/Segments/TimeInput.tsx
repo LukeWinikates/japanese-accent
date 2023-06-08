09b8e0b3843eb0dd83ee2f0902177b7b8e524556
Luke Winikates <luke.winikates@gmail.com>
2023-06-08 12:03:40 -0700

    fix arrow functions in JSX props in TimeInput.tsx

--- web/src/Video/Segments/TimeInput.tsx
+++ web/src/Video/Segments/TimeInput.tsx
@@ -1,6 +1,6 @@
 import {Button, FormControl, Input, InputLabel} from "@mui/material";
 import InputAdornment from "@mui/material/InputAdornment";
-import React from "react";
+import React, {ChangeEvent, useCallback} from "react";
 import {msToHumanReadable} from "../../App/time";
 
 interface TimeInputProps {
@@ -17,35 +17,43 @@ function parseUserInput(value: string): number {
 }
 
 export function TimeInput({value, onChange, label}: TimeInputProps) {
+  let backOneSecond = useCallback(() => onChange(value - 1000), [value, onChange]);
+  let backHalfSecond = useCallback(() => onChange(value - 500), [value, onChange]);
+  let back100ms = useCallback(() => onChange(value - 100), [value, onChange]);
+  let ahead100ms = useCallback(() => onChange(value + 100), [value, onChange]);
+  let aheadHalfSecond = useCallback(() => onChange(value + 500), [value, onChange]);
+  let aheadOneSecond = useCallback(() => onChange(value + 1000), [value, onChange]);
+  let onChangeCallback = useCallback((event:ChangeEvent<HTMLInputElement>) => onChange(parseUserInput(event.target.value)), [onChange]);
+
   return (
     <FormControl margin="normal">
       <InputLabel>{label}</InputLabel>
       <Input
         startAdornment={
           <InputAdornment position="start">
-            <Button size="small" variant="outlined" onClick={() => onChange(value - 1000)}>
+            <Button size="small" variant="outlined" onClick={backOneSecond}>
               -1s
             </Button>
-            <Button size="small" variant="outlined" onClick={() => onChange(value - 500)}>
+            <Button size="small" variant="outlined" onClick={backHalfSecond}>
               -.5s
             </Button>
-            <Button size="small" variant="outlined" onClick={() => onChange(value - 100)}>
+            <Button size="small" variant="outlined" onClick={back100ms}>
               -.1s
             </Button>
           </InputAdornment>}
         endAdornment={
           <InputAdornment position="end">
-            <Button size="small" variant="outlined" onClick={() => onChange(value + 100)}>
+            <Button size="small" variant="outlined" onClick={ahead100ms}>
               +.1s
             </Button>
-            <Button size="small" variant="outlined" onClick={() => onChange(value + 500)}>
+            <Button size="small" variant="outlined" onClick={aheadHalfSecond}>
               +.5s
             </Button>
-            <Button size="small" variant="outlined" onClick={() => onChange(value + 1000)}>
+            <Button size="small" variant="outlined" onClick={aheadOneSecond}>
               +1s
             </Button>
           </InputAdornment>}
-        onChange={(event) => onChange(parseUserInput(event.target.value))}
+        onChange={onChangeCallback}
         value={msToHumanReadable(value)}/>
     </FormControl>
   );
