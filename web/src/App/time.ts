74b8c16eacb1aed3678c2bfa53766896519206b2
Luke Winikates <luke.winikates@gmail.com>
2023-06-10 22:13:28 -0700

    fix a rounding error

--- web/src/App/time.ts
+++ web/src/App/time.ts
@@ -1,5 +1,5 @@
 export function secondsToHumanReadable(sec: number) {
-  const totalSeconds = Math.round(sec);
+  const totalSeconds = Math.floor(sec);
   const seconds = totalSeconds % 60;
   const minutes = Math.floor(totalSeconds / 60);
 
@@ -7,7 +7,7 @@ export function secondsToHumanReadable(sec: number) {
 }
 
 export function msToHumanReadable(ms: number) {
-  const totalSeconds = Math.round(ms / 1000);
+  const totalSeconds = Math.floor(ms / 1000);
   const seconds = totalSeconds % 60;
   const minutes = Math.floor(totalSeconds / 60);
 
