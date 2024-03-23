c2795c17868ca959aef93aa1eec1f4c370f1a927
Luke Winikates <luke.winikates@gmail.com>
2024-03-23 14:30:22 -0700

    up to the minimum and maximum lags

--- web/src/App/praat-algo.tsx
+++ web/src/App/praat-algo.tsx
@@ -1,9 +1,7 @@
-type Pitch = {
-
-}
+type Pitch = {}
 type Sound = {
-  sampleCount: number;
-  samplingPeriodInSeconds: number; // I think this means it's the inverse of the sample rate
+  sampleCount: number; "nx"
+  samplingPeriodInSeconds: number; // "dx" I think this means it's the inverse of the sample rate
 
 }
 
@@ -11,6 +9,7 @@ type Method = "AC_HANNING" | "AC_GAUSS" | "FCC_NORMAL" | "FCC_ACCURATE";
 
 class FFTTable {
 }
+
 
 const NUM_PEAK_INTERPOLATE_SINC70 = 3;
 const NUM_PEAK_INTERPOLATE_SINC700 = 4;
@@ -27,13 +26,13 @@ function SoundToPitchGeneric(sound: Sound,
                              octaveCost: number,
                              octaveJumpCost: number,
                              voicedUnvoicedCost: number
-) : Pitch {
+): Pitch {
 
   try {
-    let fftTable : FFTTable; // used much later, line https://github.com/praat/praat/blob/4c4f8db2ccb06d7778914024858c7279ca82f4bc/fon/Sound_to_Pitch.cpp#L447
+    let fftTable: FFTTable; // used much later, line https://github.com/praat/praat/blob/4c4f8db2ccb06d7778914024858c7279ca82f4bc/fon/Sound_to_Pitch.cpp#L447
     let t1: number;
     let numberOfFrames: number;
-    let nsampFFT : number // number of samples for FFT?
+    let nsampFFT: number // number of samples for FFT?
     let interpolationDepth: number
     let brentIxMax, brentDepth: number
     let globalPeak: number;
@@ -76,6 +75,37 @@ function SoundToPitchGeneric(sound: Sound,
       throw new Error("To analyse this Sound, “pitch floor” must not be less than " + periodsPerWindow / duration + " Hz.");
     }
 
+    /*
+      Determine the number of samples in the longest period.
+      We need this to compute the local mean of the sound (looking one period in both directions),
+      and to compute the local peak of the sound (looking half a period in both directions).
+    */
+    let nsampPeriod = Math.floor(1.0 / sound.samplingPeriodInSeconds / pitchFloor);
+    let halfNsampPeriod = nsampPeriod / 2 + 1;
+
+    pitchCeiling = Math.max(pitchCeiling, 0.5 / sound.samplingPeriodInSeconds)
+
+    /*
+        Determine window duration in seconds and in samples.
+    */
+    let dtWindow = periodsPerWindow / pitchFloor;
+    let nSampWindow = Math.floor(dtWindow / sound.samplingPeriodInSeconds);
+    let halfNsampWindow = nSampWindow / 2 - 1;
+    if (nSampWindow < 2) {
+      throw new Error("Analysis window too short.");
+    }
+
+    nSampWindow = halfNsampWindow * 2;
+
+    /*
+   * Determine the minimum and maximum lags.
+   */
+
+    // TODO: this literal "2" may be entirely wrong
+    // In the CPP original, it is 2_integer, and that value appears here:
+    // https://github.com/praat/praat/blob/4c4f8db2ccb06d7778914024858c7279ca82f4bc/external/clapack/READ_ME.TXT#L65
+    let minimumLag = Math.max(2, Math.floor(1.0 / sound.samplingPeriodInSeconds / pitchCeiling));
+    let maximumLag = Math.min(Math.floor(nSampWindow / periodsPerWindow) + 2, nSampWindow);
 
 
 
