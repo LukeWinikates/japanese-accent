d699f95d764848c49a0422d763c0fa432b97a3c3
Luke Winikates <luke.winikates@gmail.com>
2024-03-23 13:59:08 -0700

    switch statement

--- web/src/App/praat-algo.tsx
+++ web/src/App/praat-algo.tsx
@@ -10,6 +10,9 @@ type Method = "AC_HANNING" | "AC_GAUSS" | "FCC_NORMAL" | "FCC_ACCURATE";
 class FFTTable {
 }
 
+const NUM_PEAK_INTERPOLATE_SINC70 = 3;
+const NUM_PEAK_INTERPOLATE_SINC700 = 4;
+
 function SoundToPitchGeneric(sound: Sound,
                              method: Method,
                              periodsPerWindow: number,
@@ -43,6 +46,26 @@ function SoundToPitchGeneric(sound: Sound,
 
     if (dt <= 0.0) {
       dt = periodsPerWindow / pitchFloor / 4.0;   // e.g. 3 periods, 75 Hz: 10 milliseconds
+    }
+
+    switch (method) {
+      case "AC_HANNING":
+        brentDepth = NUM_PEAK_INTERPOLATE_SINC70;
+        interpolationDepth = 0.5;
+        break;
+      case "AC_GAUSS":
+        periodsPerWindow *= 2;   // because Gaussian window is twice as long
+        brentDepth = NUM_PEAK_INTERPOLATE_SINC700;
+        interpolationDepth = 0.25;   // because Gaussian window is twice as long
+        break;
+      case "FCC_NORMAL":
+        brentDepth = NUM_PEAK_INTERPOLATE_SINC70;
+        interpolationDepth = 1.0;
+        break;
+      case "FCC_ACCURATE":
+        brentDepth = NUM_PEAK_INTERPOLATE_SINC700;
+        interpolationDepth = 1.0;
+        break;
     }
 
   } catch (e) {
