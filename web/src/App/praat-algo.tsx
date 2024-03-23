ae05741e557cb9474b57fb4410ee0a7bfa26d640
Luke Winikates <luke.winikates@gmail.com>
2024-03-23 13:55:56 -0700

    up to the switch statement

--- web/src/App/praat-algo.tsx
+++ web/src/App/praat-algo.tsx
@@ -7,6 +7,9 @@ type Sound = {
 
 type Method = "AC_HANNING" | "AC_GAUSS" | "FCC_NORMAL" | "FCC_ACCURATE";
 
+class FFTTable {
+}
+
 function SoundToPitchGeneric(sound: Sound,
                              method: Method,
                              periodsPerWindow: number,
@@ -22,6 +25,25 @@ function SoundToPitchGeneric(sound: Sound,
 ) : Pitch {
 
   try {
+    let fftTable : FFTTable; // used much later, line https://github.com/praat/praat/blob/4c4f8db2ccb06d7778914024858c7279ca82f4bc/fon/Sound_to_Pitch.cpp#L447
+    let t1: number;
+    let numberOfFrames: number;
+    let nsampFFT : number // number of samples for FFT?
+    let interpolationDepth: number
+    let brentIxMax, brentDepth: number
+    let globalPeak: number;
+
+    console.assert(maxCandidatesNeeded >= 2);
+    // skip a type assertion that we don't need.
+
+    // Praat's Melder_ifloor does some constraint checking and returns an actual int value
+    if (maxCandidatesNeeded < pitchCeiling / pitchFloor) {
+      maxCandidatesNeeded = Math.floor(pitchCeiling / pitchFloor);
+    }
+
+    if (dt <= 0.0) {
+      dt = periodsPerWindow / pitchFloor / 4.0;   // e.g. 3 periods, 75 Hz: 10 milliseconds
+    }
 
   } catch (e) {
     throw new Error("pitch analysis not completed")
