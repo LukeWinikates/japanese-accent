125fee28f905a9a67b236c22cebfc4d55298111e
Luke Winikates <luke.winikates@gmail.com>
2024-03-23 13:42:14 -0700

    begin porting Praat pitch calculator to TypeScript
    * method signature and empty type definitions

---
+++ web/src/App/praat-algo.tsx
@@ -0,0 +1,26 @@
+type Pitch = {
+
+}
+type Sound = {
+
+}
+
+type Method = "AC_HANNING" | "AC_GAUSS" | "FCC_NORMAL" | "FCC_ACCURATE";
+
+function SoundToPitchGeneric(sound: Sound,
+                             method: Method,
+                             periodsPerWindow: number,
+                             dt: number,
+                             pitchFloor: number,
+                             pitchCeiling: number,
+                             maxCandidatesNeeded: number,
+                             silenceThreshold: number,
+                             voicingThreshold: number,
+                             octaveCost: number,
+                             octaveJumpCost: number,
+                             voicedUnvoicedCost: number
+) : Pitch {
+
+
+  return {};
+}
